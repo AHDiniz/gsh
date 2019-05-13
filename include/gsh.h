9e4b0de62bbb7b4599b1a9dc24e3601db5de47e3
Alan Herculano Diniz <alanhdiniz@pop-os.localdomain>
2019-05-13 17:05:22 -0300

    Defining the shell's interface

--- include/gsh.h
+++ include/gsh.h
@@ -2,6 +2,28 @@
 
 #define GSH_H_
 
+/**
+ * Function that starts the shell:
+ * 
+ * Output: bool like int that tells if the creation was successful or not
+*/
+int GSH_Init();
 
+/**
+ * Function that finishes the shell:
+*/
+void GSH_Finish();
+
+/**
+ * Function that tells if the shell is still running:
+ * 
+ * Output: bool like int that tells if the shell didn't received a exit command
+*/
+int GSH_IsRunning();
+
+/**
+ * Function that reads and executes the given commands:
+*/
+void GSH_ReadAndExecute();
 
 #endif
--- src/main.c
+++ src/main.c
@@ -7,8 +7,24 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "gsh.h" // ? Makefile allows not using the path to header file to include it
 
 int main()
 {
-	return 0;
+	// Initializing the shell and checking for errors:
+	if (!GSH_Init())
+	{
+		printf("Could not initialize gsh... :(\n");
+		return EXIT_FAILURE;
+	}
+
+	while (GSH_IsRunning()) // While an exit command is not given or an error doesn't happen
+	{
+		GSH_ReadAndExecute(); // Read the commands and executing them
+	}
+
+	GSH_Finish();
+
+	return EXIT_SUCCESS;
 }
