7d0d729dec61a69ba145592ff5f77590799960dd
Rafael <rafaelbelmockpedruzzi@hotmail.com>
2019-05-18 15:41:20 -0300

    Function mywait()

--- src/gsh/internal.c
+++ src/gsh/internal.c
@@ -8,6 +8,10 @@
 
 #include "internal.h"
 
+#include <stdio.h>
+#include <wait.h>
+#include <sys/types.h>
+
 int GSH_Exit()
 {
 	return 1;
@@ -15,5 +19,14 @@ int GSH_Exit()
 
 void GSH_MyWait()
 {
-	
+	pid_t pid;
+	do {
+		pid = waitpid(-1, NULL, WNOHANG);
+		if(pid == -1) goto wait_error;
+	} while(pid > 0);
+
+	return;
+
+	wait_error:
+	fprintf(stderr, "OOPS :O... Looks like there was an error while trying to free zombies (obviously).\n");
 }
